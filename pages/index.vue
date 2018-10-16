c7c18bdcd0ffffdb377015a753fdc60b9da95a61
Paul McNamee <paulmc121@gmail.com>
2018-10-15 23:05:59 -0600

    fixed text overflow in about section

--- pages/index.vue
+++ pages/index.vue
@@ -2,15 +2,6 @@
     <v-container>
         <section>
             <v-card class="elevation-2">
-                <br>
-                <h1 class="white--text my-3 display-1 text-xs-center">Hello.</h1>
-                <div class="subheading my-3 text-xs-center">Welcome to Paul McNamee's website.</div>
-                <br>
-            </v-card>
-        </section>
-        <br>
-        <section>
-            <v-card class="elevation-2">
                 <span ma2>
                     <v-container>
                         <v-layout>
@@ -28,16 +19,19 @@
         <section>
             <v-card class="elevation-2">
                 <v-container>
-                    <v-layout row wrap align-center>
+                    <v-layout row wrap align-center justify-center>
                         <v-flex xs12>
                             <h2>About Me</h2>
                         </v-flex>
-                        <v-flex xs12 md6 lg6 mr2>
-                            <img src="/images/selfie.jpg" width=100%>
+                        <v-flex xs12 md6 lg6>
+                            <img src="/images/selfie.jpg" width=100% class="mr-5 my-2">
                         </v-flex>
-                        <v-flex xs12 md6 lg6 ml2>
-                                <p>I create websites for small businesses and entrepreneurs to establish an online presence and fluorish in the digital age.</p>
+                        <v-flex xs12 md6 lg6>
+                            <v-flex class="ml-3">
+<p>I create websites for small businesses and entrepreneurs to establish an online presence and fluorish in the digital age.</p>
                                 <p>I am currently 28 years old, and live in Denver Colorado. I have had a huge passion for computers, programming, gaming, mechanical things, and basically anything relating to how things work since I was pretty young. I have always been a hard worker but also a smart worker, I automate the stuff I don't really want to do, or figure out a way to have someone or something else do it. I love programming, but my interests definitely extend way beyond computers. Fun fact, I was heavily considering doing ceramics full time as a career path. I also like to hike, fish, snowboard, make beer (and drink it), and lots of other stuff. Check out the about page or the blog posts to get to know me better.</p>
+                            </v-flex>
+                                
                         </v-flex>
                     </v-layout>
                 </v-container>
