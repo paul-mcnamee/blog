7206d6c07525e1254edbfcc5d013ef042f1f9d4b
Paul McNamee <paulmc121@gmail.com>
2020-06-07 21:33:52 -0600

    Update link for twitter

--- components/Social.vue
+++ components/Social.vue
@@ -1,15 +1,24 @@
 <template>
-    <v-layout layout row justify-center align-center>
-        <a class="social mx-2" href="mailto:Paul@FourthMouse.com"><v-icon>email</v-icon></a>
-        <a class="social mx-2" href="https://github.com/paul-mcnamee"><v-icon>fab fa-github</v-icon></a>
-        <a class="social mx-2" href="https://www.linkedin.com/in/paul-j-mcnamee/"><v-icon>fab fa-linkedin</v-icon></a>
-        <a class="social mx-2" href="https://twitter.com/pmcnamee_"><v-icon>fab fa-twitter</v-icon></a>
-        <a class="social mx-2" href="https://stackoverflow.com/users/9683122/pmcnamee"><v-icon>fab fa-stack-overflow</v-icon></a>
-    </v-layout>
+  <v-layout layout row justify-center align-center>
+    <a class="social mx-2" href="mailto:Paul@FourthMouse.com">
+      <v-icon>email</v-icon>
+    </a>
+    <a class="social mx-2" href="https://github.com/paul-mcnamee">
+      <v-icon>fab fa-github</v-icon>
+    </a>
+    <a class="social mx-2" href="https://www.linkedin.com/in/paul-j-mcnamee/">
+      <v-icon>fab fa-linkedin</v-icon>
+    </a>
+    <a class="social mx-2" href="https://twitter.com/pmacdev">
+      <v-icon>fab fa-twitter</v-icon>
+    </a>
+    <a class="social mx-2" href="https://stackoverflow.com/users/9683122/pmcnamee">
+      <v-icon>fab fa-stack-overflow</v-icon>
+    </a>
+  </v-layout>
 </template>
 
 <style scoped>
-
 .social :hover,
 .nav-link a:hover,
 .nav-link a:active,
